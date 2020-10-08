4329fc1109dd8e77586386459237e05555c45369
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-08 05:15:16 +0200

    download cv

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,7 +12,8 @@
                 <div class="column right">
                   
                     <p>Ndumiso Tshabalala is a final year BSc Information Technology student at the North-West University, passionate about telecommunications networks. Highly capable leader, having led multiple senior class projects to completion. Proficient in a range of modern technologies. Ndumiso Tshabalala was born in a small town of Ficksburg, Freestate. He grew up with various talents and skills, one that can be highlighted is soccer. He played position six and was always well known and trusted for his excellent performance. As of now, one major hobby is weightlifting/bodybuilding, which basically was always intrigued by the need to look good and be healthy. As a child he was always fascinated about computers, and thus owned a computer from a young age. He went on to be top his fellow classmates in his computer subjects on numerous occasions along the years, this made him a lot more interested in pursuing this degree.</p>
-                    <a href="#">Download CV</a>
+                    <a href = "other/cv.pdf" download="Ndumiso's CV">Download CV</a>
+                    
                 </div>
             </div>
         </div>
