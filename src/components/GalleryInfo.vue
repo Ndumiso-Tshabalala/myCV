6c0c65ffd58e263ba828c8cd71996fdab092c60c
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-09 03:31:54 +0200

    Create GalleryInfo.vue

---
+++ src/components/GalleryInfo.vue
@@ -0,0 +1,36 @@
+<template>
+ <div id="app">
+  <!-- Gallery section -->   
+    
+     <section class="gallery" id="gallery">
+        
+             <h2 class="title"  > My interests & activities </h2>
+              <div class = "holdingcontainer">
+                  <div class = "internalcontainerL"> cwrgwrugnh oiurcrythgt srturnsgtuir tsrntisbuet rugst strshutbggrs rsngb srgss ssnrgwrgwrugnh oiurcrythgt srturnsgtuir tsrntisbuet rugst strshutbggrs rsngb srgss ssnrg</div>
+                  <div class = "internalcontainerR"> cwrgwrugnh oiurcrythgt srturnsgtuir tsrntisbuet rugst strshutbggrs rsngb srgss ssnrgwrgwrugnh oiurcrythgt srturnsgtuir tsrntisbuet rugst strshutbggrs rsngb srgss ssnrg</div>
+                
+              </div>
+
+             
+     </section>
+                         
+   
+
+    
+ </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "GalleryInfo"
+
+}
+</script>
+
+
+
+<style scooped>
+
+</style>
