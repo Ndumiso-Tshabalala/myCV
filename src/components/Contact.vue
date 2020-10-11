a08eb7076f5a532cfebab55d5ef2ac96f42d961a
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-11 17:09:42 +0200

    Create Contact.vue

---
+++ src/components/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+ <div id="app">
+  <!-- Contact section -->   
+   <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    Hiring? need a project hand. Do not hesitate to get in touch. I am relatively active 24/7.
+                    <div class="icons">
+                        <div class="row">
+                           
+
+    
+ </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "Contact"
+
+}
+</script>
+
+
+
+<style scooped>
+
+</style>
