480cbd75fc65da5a5bdb5c1cfb9cc900dad12ebb
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-11 23:46:42 +0200

    Update GalleryInfo.vue
    Creating Youtube API

--- src/components/GalleryInfo.vue
+++ src/components/GalleryInfo.vue
@@ -1,10 +1,26 @@
 <template>
  <div id="app">
-  <!-- Gallery section -->   
+  <!-- Gallery section / Youtube search-->   
     
      <section class="galleryInfo" id="galleryInfo">
         
              <h2 class="title"  > My interests & activities </h2>
+               <nav class="navbar navbar-dark bg-danger">
+        <a class="navbar-brand" href="#">Youtube Data API V3</a>
+    </nav>
+    <div class="container">
+        <form id="searchForm">
+            <div class="input-group py-4">
+                <input type="text" class="form-control" id="searchField">
+                <div class="input-group-prepend">
+                    <button type="submit" class="btn btn-danger">Search</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="container-fluid mb-4">
+        <div class="row">
+
              
              
 
