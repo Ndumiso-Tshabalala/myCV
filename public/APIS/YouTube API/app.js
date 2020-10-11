b29707d3f4944e816f5fbfd01a706def66beea22
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-12 01:04:21 +0200

    Create app.js

---
+++ public/APIS/YouTube API/app.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    let searchTerm = "gamerunss";
+    let videoList = $("#videoList");
+    let apiKey = "AIzaSyCi7u_Px2g8KVGGnhbga9J0NzzjT2j8Hkg";
+    let player = $("#player");
+    clearVideoList();
+    search(searchTerm);
+
+    $("#searchForm").on("submit", (e) => {
+        e.preventDefault();
+        searchTerm = $("#searchField").val();
+        search(searchTerm);
+    });
+
+  
