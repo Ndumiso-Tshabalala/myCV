6bba1e268441a259a709048a73cd3b9b47ad0fd9
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-11 23:20:55 +0200

    Update Details.vue

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,54 +1,53 @@
 <template>
  <div id="app">
-     <!-- personal details section -->
-       <section class="personal" id="personal">
-           <h2 class="title">Personal details</h2>
-     <div class="wrapper">
-      <div class="left">
-          <img src="images/profile-1.jpeg" 
-          alt="user" width="130">
-          <h4>Ndumiso Tshabalala</h4>
-           <p>Technician</p>
-      </div>
-      <div class="right">
-          <div class="info">
-              <h3>Information</h3>
-              <div class="info_data">
-                   <div class="data">
-                      <h4>Email</h4>
-                      <p>tshabalalandumiso3@gmail.com</p>
-                      <h4>Languages</h4>
-                      <p>English, Sesotho, Setswana, IsiZulu</p>
-                      
-                   </div>
-                   <div class="data">
-                     <h4>Phone</h4>
-                      <p>(+27)63-181-9910</p>
-                      <h4>Driver's License</h4>
-                      <p>Yes</p>
-                </div>
-              </div>
+     <!-- education and achievements details section -->
+       <section class="edu" id="edu">
+           <h2 class="title">Education & Achievements</h2>
+
+          <div class = "inner width">
+            <div class = "time-line">
+            <div class = "block">
+              
+              <h4> 2004 - Primary Education</h4>
+              <h3> Institution - Leribe English Medium School</h3>
+              <p> “The first day of school: The day when the countdown to the last day of the school begins.” – Unknown</p> 
+              
+              <h3>Achievements:  </h3>
+              <p>Perfect attendance awards</p>
+              <p>Grade 2 - top 1 in grade</p>
+              <p>Grade 7 soccer first team </p>
+
+            </div>
+
+             <div class = "block">
+               <h4> 2017 - Secondary Education</h4>
+              <h3> Institution - New Horizon College</h3>
+              <p> “High school is neither a democracy nor a dictatorship – nor, contrary to popular belief, an anarchic state. High school is a divine-right monarchy. And when the queen goes on vacation, things change.” – John Green</p>
+              <h3>Achievements:  </h3>
+              <p>Grade 11 top 5 learner in CAT</p>
+              <p>Grade 12 rugby first team</p>
+              <p>Grade 12 top 1 learner in CAT </p>
+            
+            </div>
+
+             <div class = "block">
+               <h4> 2018 - Tertiary Education</h4>
+              <h3> Institution - North-West University</h3>
+              <p> “Education is our passport to the future, for tomorrow belongs to the people who prepare for it today.” – Malcolm X</p> 
+              <h3>Achievements:  </h3>
+              <p>Juniors Student Assistant(Demi)</p>
+              <p> Top 15% achiever in Grade </p>
+              <p> Goldenkey candidate </p>
+              <p> Absa Gradstar top 100 achiever</p>
+
+
+            </div>
+
+
+
+
+            </div>
           </div>
-        
-        <div class="projects">
-              <h3>Education & Achievements</h3>
-              <div class="projects_data">
-                   <div class="data">
-                      <h4>New Horizon College</h4>
-                      <p> Completed Matric | 2017</p>
-                      <p> 2016 - Student council member <br> 2016 -Grade 11 Top 5 Learner in CAT <br> 2017 - Grade 12 Top 1 Learner In CAT</p>
-                   </div>
-                   <div class="data">
-                     <h4>North-West Univesity</h4>
-                      <p>Currently enrolled in Bsc Information Technology</p>
-                      <p> 2019 - Juniors student assistant in programming <br> 2019 - Top 15% achiever in IT<br> 2020 - Golden key achiever <br> 2020 - GradStar Awards top 100 achiever</p>
-                </div>
-              </div>
-          </div>
-        
-       
-      </div>
-  </div>
        </section>
     
  </div>
