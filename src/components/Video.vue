2a37c173c2fdd68adb436b942ba3ef5f915e05cc
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-12 07:32:25 +0200

    Create Video.vue

---
+++ src/components/Video.vue
@@ -0,0 +1,51 @@
+<template>
+ <div id="app">
+  
+  
+ 
+   <section class = "sliding">
+         <h2 class="title"  > My interests & activities </h2> 
+     <div class = "slider" id="slider">
+
+         
+           <input type = "radio" name = "slider" id = "s1" checked>
+           <input type = "radio" name = "slider" id = "s2" >
+           <input type = "radio" name = "slider" id = "s3" >
+           <input type = "radio" name = "slider" id = "s4" >
+           <input type = "radio" name = "slider" id = "s5" >
+
+            <label for = "s1" id =  "slide1">1</label>
+            <label for = "s2" id =  "slide2">2</label>
+            <label for = "s3" id =  "slide3">3</label>
+            <label for = "s4" id =  "slide4">4</label>
+            <label for = "s5" id =  "slide5">5</label>
+          
+</div>
+
+
+         
+   
+
+
+     </section>
+
+
+
+ </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "Video"
+
+}
+</script>
+
+
+
+<style scooped>
+
+
+</style>
