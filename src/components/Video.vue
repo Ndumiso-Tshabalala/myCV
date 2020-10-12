36409742e35db99cd801977e9cf2a1b0ceec6b1e
Ndumiso-Tshabalala <72162083+Ndumiso-Tshabalala@users.noreply.github.com>
2020-10-12 07:59:13 +0200

    Update Video.vue

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -9,13 +9,14 @@
 
          
            <input type = "radio" name = "slider" id = "s1" checked>
+
            <input type = "radio" name = "slider" id = "s2" >
            <input type = "radio" name = "slider" id = "s3" >
            <input type = "radio" name = "slider" id = "s4" >
            <input type = "radio" name = "slider" id = "s5" >
 
-            <label for = "s1" id =  "slide1">1</label>
-            <label for = "s2" id =  "slide2">2</label>
+            <label for = "s1" id =  "slide1"><iframe width src="https://www.youtube.com/embed/_DX2L4Pp8S0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></label>
+            <label for = "s2" id =  "slide2"><iframe width src="https://www.youtube.com/embed/eAagtcAup0o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></label>
             <label for = "s3" id =  "slide3">3</label>
             <label for = "s4" id =  "slide4">4</label>
             <label for = "s5" id =  "slide5">5</label>
